5b10b809b0df333d1cceee5ae55311aa93fdeea0
manfredy21 <manfredyantonioescobar@gmail.com>
2024-06-25 22:23:09 -0600

    Learn activity done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction myFraction1 = new Fraction();
+        Fraction myFraction2 = new Fraction(5);
+        Fraction myFraction3 = new Fraction(3,4);
+        Fraction myFraction4 = new Fraction(1,3);
+
+        Console.WriteLine(myFraction1.GetFractionString());
+        Console.WriteLine(myFraction1.GetDecimalValue());
+        Console.WriteLine(myFraction2.GetFractionString());
+        Console.WriteLine(myFraction2.GetDecimalValue());
+        Console.WriteLine(myFraction3.GetFractionString());
+        Console.WriteLine(myFraction3.GetDecimalValue());
+        Console.WriteLine(myFraction4.GetFractionString());
+        Console.WriteLine(myFraction4.GetDecimalValue());
+        
     }
 }
